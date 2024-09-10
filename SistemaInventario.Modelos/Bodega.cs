1b9d4d3e6ae15b878b2f9b6e448e7bdb62cf977f
Adrian Lavigne <adrianlavig@gmail.com>
2024-09-10 23:48:23 +0200

    primeras migraciones

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [MaxLength(60, ErrorMessage = "Permite hasta 60 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripcion es obligatorio")]
+        [MaxLength(100, ErrorMessage = "Permite hasta 100 caracteres")]
+        public string Descripcion { get; set;}
+
+        [Required(ErrorMessage = "Estado es obligatorio")]
+        public bool Estado { get; set;}
+    }
+}
